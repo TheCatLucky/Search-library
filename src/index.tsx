8aa09fed5c304759c7270c86a0e4c49de3f39e4a
TheCatLucky <nervousmumy@gmail.com>
2022-02-14 21:58:10 +0300

    Настройка окружения

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import "@ff/ui-kit/lib/styles/fns.theme.css";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
